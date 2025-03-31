555b4c84a1538c5bf14bc61d0b0102ca39783f5d
paraggit-tech <parag.panda1987@gmail.com>
2025-03-31 12:56:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  count         = 2
+  ami           = "ami-02f624c08a83ca16f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "hcp-server"
+  }
+}
