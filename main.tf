aea4241048cdd5bdbbcd01b8988a24af3e6e3275
paraggit-tech <parag.panda1987@gmail.com>
2025-03-31 13:45:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_instance" "one" {
-  count         = 2
   ami           = "ami-02f624c08a83ca16f"
   instance_type = "t2.micro"
   tags = {
